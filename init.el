78864e1d7434153040485f2931157a21e67de781
Richard Fieldsend <richardfieldsend@gmail.com>
2019-05-10 00:53:03 +0100

    Removed time-stamp from init.el because file keeps on updating.

--- init.el
+++ init.el
@@ -1,8 +1,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; init.el file
 ;;
-;; Time-stamp: <2019-05-10 00:32:24 rf343>
-;;
 ;; This file is used in starting up Emacs. In my setup this is the
 ;; first file read, though I know that there is now a file which comes
 ;; before init.el if it exists. I may investigate that one day.
@@ -11,6 +9,10 @@
 ;; it does enable it by calling the config.org file using org-babel
 ;; function. Information on how the literate programming paradigm is
 ;; intended to work can be found in config.org.
+;;
+;; I have removed the time-stamp entry because this file would need
+;; adding into the repository repeatedly even though it wasn't
+;; actually changing.
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Link the config.org file (which will generate the config.el file).
