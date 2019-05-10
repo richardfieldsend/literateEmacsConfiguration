f48527025123eac1d2c1db1f5ed5d8600dc1c945
Richard Fieldsend <richardfieldsend@gmail.com>
2019-05-10 23:07:52 +0100

    Added a lot of placeholder stuff about things I want to add.

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (rainbow-delimiters use-package cyberpunk-theme))))
+ '(package-selected-packages '(rainbow-delimiters use-package cyberpunk-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
