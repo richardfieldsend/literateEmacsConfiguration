d9d33a5c513830a269fa5f8abce7f1fed1b6c230
Richard Fieldsend <richardfieldsend@gmail.com>
2019-05-17 00:55:31 +0100

    Add texlint mode

--- init.el
+++ init.el
@@ -22,7 +22,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(rainbow-delimiters use-package cyberpunk-theme)))
+ '(package-selected-packages
+   '(flycheck writeroom-mode use-package speed-type rainbow-delimiters paredit palimpsest org-journal org-bullets minimap magit-popup magit htmlize helm ghub doom-themes doom-modeline cyberpunk-theme cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
